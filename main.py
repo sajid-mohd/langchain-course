ba9e4985d926636d078663d4c909ad7edf1464aa
Mohammad Sajid <mdsajid2152@gmail.com>
2026-06-22 02:41:58 +0530

    Env setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!")
+    print(os.environ.get("GOOGLE_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
